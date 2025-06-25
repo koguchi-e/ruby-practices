353fa72dedda3d7366092a47c80746666e4e8d38
koguchi.e <koguchi.e915@gmail.com>
2025-06-25 12:20:00 +0900

    rubocopのエラー内容を修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'debug'
 
 # score：入力した点数を取得
@@ -11,13 +13,8 @@ scores = score.split(',')
 # shotsという配列に入れる
 shots = []
 scores.each do |s|
-  if s == 'X'
-    shots << 10
-  else
-    shots << s.to_i
-  end
+  shots << (s == 'X' ? 10 : s.to_i)
 end
-p shots
 
 ############################################
 
@@ -28,7 +25,7 @@ p shots
 index = 0
 frames = []
 # framesが10以下まで計算を続ける
-while frames.size < 10 do
+while frames.size < 10
   if shots[index] == 10
     # ストライクなら10を入れて終了、2投はなし
     frames << [10]
@@ -36,7 +33,7 @@ while frames.size < 10 do
   else
     # それ以外は次の数を2投目としてセット
     # || 0 : 値がなければ0を使う
-    frames << [shots[index], shots[index +1] || 0]
+    frames << [shots[index], shots[index + 1] || 0]
     index += 2
   end
 end
@@ -50,8 +47,6 @@ if frames[9].sum >= 10
   frames[9] << shots[index + 1] || 0 if frames[9][0] == 10
 end
 
-p frames
-
 ################################################
 
 ## ここから得点計算
@@ -60,7 +55,6 @@ point = 0
 
 frames[0..9].each_with_index do |frame, i|
   if i == 9
-    puts "10フレーム目：#{point} + #{frame.sum}"
     point += frame.sum
     next
   end
@@ -72,22 +66,16 @@ frames[0..9].each_with_index do |frame, i|
 
     next_number = all_throws[throw_index + 1] || 0
     next_number2 = all_throws[throw_index + 2] || 0
-    puts "ストライク：#{point} + #{frame[0]} + #{next_number} + #{next_number2}"
-    point += 10 + next_number + next_number2 
-    puts point
+    point += 10 + next_number + next_number2
 
   # スペア
   elsif frame.sum == 10
     next_number = frames[i + 1] ? frames[i + 1][0] : 0
-    puts "スペア：#{point} + 10 + next_number(#{next_number})"
     point += 10 + next_number
-    puts point
-    
-  # それ以外
+
+  # それ以外　
   else
-    puts "それ以外：#{point} + #{frame.sum}"
     point += frame.sum
-    puts point
   end
 end
 
