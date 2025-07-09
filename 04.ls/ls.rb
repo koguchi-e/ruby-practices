969a78c8cbcfcdee20f910c0ad45a497bf7e34d6
koguchi-e <koguchi.e915@gmail.com>
2025-07-09 13:56:54 +0900

    縦並びに配置

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,11 +1,22 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
-
-# !/usr/bin/env ruby
 
 files = Dir.children('.').reject { |f| f.start_with?('.') }
 
 sorted = files.sort_by do |f|
-  File.directory?(f) ? 0 : 1
+  [File.directory?(f) ? 0 : 1, f.downcase]
 end
 
-puts sorted.join('  ')
+rows = ( sorted.size.to_f / 3).ceil
+
+columns_array = Array.new(3) { [] }
+
+sorted.each_with_index do |file, index|
+  col = index / rows
+  columns_array[col] << file
+end
+
+(0...rows).each do |row_idx|
+  line = columns_array.map { |col| col[row_idx] || '' }.map { |name| name.ljust(20) }.join
+  puts line.rstrip
+end
