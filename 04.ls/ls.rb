2280c88cbd21dc0ae74be8131eab12edc7f0c2b7
koguchi-e <koguchi.e915@gmail.com>
2025-07-16 08:28:11 +0900

    -a用コード追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-files = Dir['*']
+show_all = ARGV.include?("-a")
+files = show_all ? Dir.entries(".") : Dir["*"]
 
 sorted = files.sort_by do |f|
   [File.directory?(f) ? 0 : 1, f.downcase]
