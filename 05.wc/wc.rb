1c62301fe978b552aacee9eadb937ba0d2952414
koguchi-e <koguchi.e915@gmail.com>
2025-08-05 14:43:10 +0900

    ワード数(-w)を出力

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -2,12 +2,20 @@
 # frozen_string_literal: true
 
 show_line_count = ARGV.include?('-l')
+show_word_count = ARGV.include?('-w')
 
-file = File.join(__dir__, "file1.txt")
+file = File.join(__dir__, "file3.txt")
 
 def print_line_count(file)
   line_count = File.readlines(file).size
   puts "#{line_count} #{file}"
 end
 
+def print_word_count(file)
+  text = File.read(file)
+  word_count = text.split.size
+  puts "#{word_count} #{file}"
+end
+
 print_line_count(file) if show_line_count
+print_word_count(file) if show_word_count
