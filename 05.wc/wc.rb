bd5885322c6676f4317896915d347d391134f2a6
koguchi-e <koguchi.e915@gmail.com>
2025-08-06 12:50:27 +0900

    lsコマンドファイルと一緒に処理できるよう修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -11,22 +11,34 @@ if !show_line_count && !show_word_count && !show_byte_count
   show_line_count = show_word_count = show_byte_count = true
 end
 
-def print_counts(file, show_line_count, show_word_count, show_byte_count)
-  text = File.read(file)
+def count_from_text(text, name = '')
   line_count = text.lines.count
   word_count = text.split.size
   byte_count = text.bytesize
 
   output = []
-  output << line_count if show_line_count
-  output << word_count if show_word_count
-  output << byte_count if show_byte_count
-  output << file
+  output << line_count if $show_line_count
+  output << word_count if $show_word_count
+  output << byte_count if $show_byte_count
+  output << file unless name.empty?
   puts output.join(' ')
 end
 
-file_names.each do |file|
-  if File.exist?(file)
-    print_counts(file, show_line_count, show_word_count, show_byte_count)
+$show_line_count = show_line_count
+$show_word_count = show_word_count
+$show_byte_count = show_byte_count
+
+if file_names.empty?
+  input_text = $stdin.read
+  count_from_text(input_text)
+else
+  file_names.each do |file|
+    if File.exist?(file)
+      text = File.read(file)
+      count_from_text(text, file)
+    else
+      warn "#{file}: No such file"
+    end
   end
 end
+
