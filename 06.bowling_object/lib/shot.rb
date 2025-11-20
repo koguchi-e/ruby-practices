c5de8b49bd996ea0fb3db95fd8b3d70dfefbb708
koguchi-e <koguchi.e915@gmail.com>
2025-11-20 16:42:09 +0900

    Shotクラスのtransformeメソッドをよりシンプルにする

--- 06.bowling_object/lib/shot.rb
+++ 06.bowling_object/lib/shot.rb
@@ -2,12 +2,6 @@
 
 class Shot
   def transforme
-    read.map { |s| s == 'X' ? 10 : s.to_i }
-  end
-
-  private
-
-  def read
-    ARGV[0].split(',')
+    ARGV[0].split(',').map { |s| s == 'X' ? 10 : s.to_i }
   end
 end
