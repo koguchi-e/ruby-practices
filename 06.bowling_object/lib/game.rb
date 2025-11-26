ffcee113d2c86fb15e975bc5e1716f49dedd9034
koguchi-e <koguchi.e915@gmail.com>
2025-11-26 16:58:33 +0900

    OOPに切り分けた状態でGameクラスを保存

--- 06.bowling_object/lib/game.rb
+++ 06.bowling_object/lib/game.rb
@@ -5,12 +5,50 @@ require_relative './shot'
 require_relative './frame'
 
 class Game
-  def calculate_score
+  def initialize(shots, frames)
+    @shots = []
+    @frames = []
+  end
+  def make_shots
     input_value = ARGV[0].split(',').map { |s| s == 'X' ? 10 : s.to_i }
-    shots = input_value.map { |pin| Shot.new(pins) }
-    frames << Frame.new(shots[i], shots[i+1])
-    
-    
+    @shots = input_value.map { |pin| Shot.new(pin) }
+  end
+
+  def make_frames
+    i = 0
+    while @frames.length < 9
+      if @shots[i].score == 10
+        frame = Frame.new(@shots[i])
+        @frames << frame
+        i += 1
+      else
+        frame = Frame.new(@shots[i], @shots[i+1])
+        @frames << frame
+        i += 2
+      end
+    end
+    last_frame = Frame.new( @shots[i], @shots[i+1], @shots[i+2] )
+    @frames << last_frame
+  end
+
+  def bonus(frame_index)
+    frame = @frames[frame_index]
+
+    if frame.strike?
+      bonus = @shots[frame_index + 1].score + @shots[frame_index + 2].score
+      return bonus
+    elsif frame.spare?
+      bonus = @shots[frame_index + 2].score
+      return bonus
+    else
+      return 0
+    end
+  end
+
+  def total_score
+    @frames.each_with_index.sum do |frame, i|
+      frame.frame_score + bonus(i)
+    end
   end
 end
 
