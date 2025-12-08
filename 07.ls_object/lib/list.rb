b16f62095309b6880c4a6fba83740f2d8679432a
koguchi-e <koguchi.e915@gmail.com>
2025-12-08 16:36:39 +0900

    Entryクラスの作成、-lの処理を追加

--- 07.ls_object/lib/list.rb
+++ 07.ls_object/lib/list.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# require_relative './entry'
+require_relative './entry'
 # require_relative './file'
 # require_relative './directory'
 
@@ -9,14 +9,11 @@ require 'etc'
 
 class List
   def initialize
-    # どのオプションがオンか？オプション状態を持つ
     parse_options
   end
 
   # どのオプションが true/false かを決める・解析するだけ
   def parse_options
-    # ARGVでオプションを受け取る
-    # オプション（a, r, l）を設定する
     options = ARGV.flat_map do |argument|
       argument.start_with?('-') ? argument[1..].chars.map { |c| "-#{c}" } : argument
     end
@@ -29,63 +26,45 @@ class List
   # ファイル一覧を受け取る
   def load_entries
     if @show_all
-      @enties = Dir.entries('.')
+      @entries = Dir.entries('.')
+      puts @enties
     else
-      @enties = Dir['*']
+      @entries = Dir['*']
     end
 
-    @enties.sort_by!(&:downcase)
-    @enties.reverse! if @show_reverse
-    pp @enties
+    @entries.sort_by!(&:downcase)
+    puts @entries.reverse! if @show_reverse
+
+    if @show_list
+      puts "total #{calc_total_blocks}"
+      entries_list = @entries.map { |name| Entry.new(name) }
+    end
   end
-
-  # 解析に基づき実行する
-  # def execute_options
-  #   @enties.reverse! if @show_reverse
-
-  #   if @show_list
-  #     show_list_format
-  #   else
-  #     show_column_format
-  #   end
-  # end
 
   # # -lで出す詳細情報
   # def show_list_format
-  #   # この情報はEntryクラスに持たせる
-  #   # listクラスでは出力だけ
-
-  #   # puts "total #{calc_total_blocks(@enties)}"
-
-  #   # @enties.each do |file|
-  #   #   stat = File.stat(file)
-  #   #   mode = stat.mode
-  #   #   link = stat.nlink
-  #   #   user = Etc.getpwuid(stat.uid).name
-  #   #   group = Etc.getgrgid(stat.gid).name
-  #   #   size = stat.size
-  #   #   time = stat.mtime.strftime('%b %e %H:%M')
-  #   #   perm = permission_string(mode)
-  #   #   name = file
-
-  #   #   printf "%<perm>s %<link>2d %<user>-8s %<group>-8s %<size>4d %<time>s %<name>s\n",
-  #   #         perm:,
-  #   #         link:,
-  #   #         user:,
-  #   #         group:,
-  #   #         size:,
-  #   #         time:,
-  #   #         name:
-  #   # end
+  #   # List は 複数の Entry を並べるだけ。
+  #   # Entry.newで一個一個のエントリを生成（Entryはファイル情報を持つ）
+  #   # それを並べて出力するだけ（mapを使う）
+  #   entries = filenames.map { |name| Entry.new(name) }
+  #   puts "total #{calc_total_blocks}"
+  #   puts entries
   # end
+
+  def calc_total_blocks
+    total_files = @entries.sum do |file|
+      File.exist?(file) ? File.stat(file).blocks : 0
+    end
+    (total_files / 2.0).ceil
+  end
 
   # # -l以外の表示
   # def show_column_format
   #   cells = 3
-  #   row_count = (@enties.size.to_f / cells).ceil
+  #   row_count = (@entries.size.to_f / cells).ceil
   #   columns = Array.new(cells) { [] }
 
-  #   @enties.each_with_index do |file, index|
+  #   @entries.each_with_index do |file, index|
   #     col = index/ row_count
   #     columns[col] << file
   #   end
@@ -93,6 +72,15 @@ class List
   #   row_count.times do |row_idx|
   #     line = columns.map { |col| col[row_idx] || ' ' }.map { |name| name.ljust(20) }.join
   #     puts line.rstrip
+  #   end
+  # end
+
+  # 解析に基づき実行する
+  # def execute_options
+  #   if @show_list
+  #     show_list_format
+  #   else
+  #     show_column_format
   #   end
   # end
 end
