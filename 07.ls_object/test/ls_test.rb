f6d74b601d4d3bea57b1c2980d4c7c6075a6eb05
koguchi-e <koguchi.e915@gmail.com>
2025-12-09 16:56:43 +0900

    テストファイルの作成

--- 07.ls_object/test/ls_test.rb
+++ 07.ls_object/test/ls_test.rb
@@ -3,9 +3,56 @@
 
 require 'minitest/autorun'
 require_relative '../lib/list'
+require 'pathname'
 
 class ListTest < Minitest::Test
-  # 動作確認用ファイル
-  # ヒアドキュメントでケースごとに同じ形になってるかをテスト
+  TEST_PATH = Pathname('./fixtures')
 
+  def setup
+    @old_argv = ARGV.dup
+  end
+
+  def replace
+    ARGV.replace(@old_argv)
+  end
+
+  def make_lists(arg)
+    ARGV.replace(arg)
+
+    Dir.chdir(TEST_PATH) do
+      stdout, _stderr = capture_io do
+        List.new.output_list
+      end
+      return stdout
+    end
+  end
+
+  def test_all
+    expected = <<~TEXT
+      .                   Dir2                file3.txt
+      ..                  file1.txt
+      Dir1                file2.txt
+    TEXT
+    assert_equal expected, make_lists(['-a'])
+  end
+
+  def test_reverse
+    expected = <<~TEXT
+      file3.txt           file1.txt           Dir1
+      file2.txt           Dir2
+    TEXT
+    assert_equal expected, make_lists(['-r'])
+  end
+
+  def test_lists
+    expected = <<~TEXT
+      total 20
+      drwxr-xr-x  2 koguchi  koguchi  4096 Dec  8 14:39 Dir1
+      drwxr-xr-x  2 koguchi  koguchi  4096 Dec  8 14:39 Dir2
+      -rw-r--r--  1 koguchi  koguchi   101 Dec  8 14:36 file1.txt
+      -rw-r--r--  1 koguchi  koguchi   102 Dec  8 14:36 file2.txt
+      -rw-r--r--  1 koguchi  koguchi   102 Dec  8 14:38 file3.txt
+    TEXT
+    assert_equal expected, make_lists(['-l'])
+  end
 end
